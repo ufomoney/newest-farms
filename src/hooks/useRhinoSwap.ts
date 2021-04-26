9648a2adde6d4c073929e0a6f0e5062ca9fb750a
Jon Willis <jondwillis@gmail.com>
2021-04-25 23:50:26 -0400

    fix panda withdrawable balance

--- src/hooks/useRhinoSwap.ts
+++ src/hooks/useRhinoSwap.ts
@@ -6,6 +6,7 @@ import {
   getRhinoStakingContract,
   getWithdrawableBalance,
   getRhinoContract,
+  getPandaAddress,
 } from '../panda/utils'
 import BigNumber from 'bignumber.js'
 
@@ -24,11 +25,20 @@ export const useRhinoSwapWithdrawableBalance = (panda: Panda): BigNumber => {
   ])
 
   const fetchRhinoWithdrawableBalance = useCallback(async () => {
-    const balance = await getWithdrawableBalance(
+    let balance = await getWithdrawableBalance(
       rhinoStakingContract,
       account,
       getRhinoContract(panda)?.options.address,
     )
+    if (balance.isGreaterThan(0)) {
+      setWithdrawableBalance(new BigNumber(balance))
+    } else {
+      balance = await getWithdrawableBalance(
+        rhinoStakingContract,
+        account,
+        getPandaAddress(panda),
+      )
+    }
     setWithdrawableBalance(new BigNumber(balance))
   }, [panda, account, rhinoStakingContract])
 
