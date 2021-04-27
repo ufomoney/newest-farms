d4863069734044d00287f1ac731593c744927f31
Vitalsine <rob@creative-element.net>
2021-04-27 02:29:20 -0400

    fixed spelling error

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -50,13 +50,13 @@ const Home: React.FC = () => {
 				revenue for the Panda Treasury which PNDA holders own. <br />
 			</StyledInfo>
 			<Spacer size="md" />
-			<StyledInfo>
-			❗️ <span style={{fontWeight: 600, color: 'red'}}>Attention:</span> Please familiarize yourself with the fee structure before using PandaChef. 
-			Deposits are subject to a 0.75% fee. Withdrawal slashing fee of 0.1% - 50 % will be incurred 
-			when exiting a farm, a depending on the length of time your LP was staked. Please <StyledLink href="https://docs.bao.finance/franchises/panda/pandaswap-fees-penalties" target="blank"> read 
-			the docs</StyledLink> to familiarize yourself with fees and penalties.
-</StyledInfo>
-<Spacer size="md" />
+				<StyledInfo>
+					❗️ <span style={{ fontWeight: 600, color: 'red' }}>Attention:</span> Please familiarize yourself with the fee structure before using PandaChef.
+					Deposits are subject to a 0.75% fee. Withdrawal slashing fee of 0.1% - 50 % will be incurred
+					when exiting a farm, depending on the length of time your LP was staked. Please <StyledLink href="https://docs.bao.finance/franchises/panda/pandaswap-fees-penalties" target="blank"> read
+					the docs</StyledLink> to familiarize yourself with fees and penalties.
+				</StyledInfo>
+			<Spacer size="md" />
 		</Page>
 	)
 }
