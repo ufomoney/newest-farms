cba21f8e2e695fedb36d6b27dc778e89925fcc15
Vitalsine <rob@creative-element.net>
2021-05-13 02:40:56 -0400

    start

--- src/views/Farm/components/WithdrawModal.tsx
+++ src/views/Farm/components/WithdrawModal.tsx
@@ -7,6 +7,7 @@ import ModalTitle from '../../../components/ModalTitle'
 import ModalContent from '../../../components/ModalContent'
 import TokenInput from '../../../components/TokenInput'
 import { getFullDisplayBalance } from '../../../utils/formatBalance'
+import styled from 'styled-components'
 
 interface WithdrawModalProps extends ModalProps {
 	max: BigNumber
@@ -61,8 +62,15 @@ const WithdrawModal: React.FC<WithdrawModalProps> = ({
 					}}
 				/>
 			</ModalActions>
+			<StyledDocsWarning>
+				<p>First Deposit Block:</p>
+				<p>Last Withdraw Block:</p>
+				<p>Current Block:</p>
+				<p>Withdraw Fee:</p>
+			</StyledDocsWarning>
 			<ModalContent>
 				{
+
 					'Remember the longer you stay in a pool the lower your fee. Read the docs for details, but most users will want to stay in a pool 4 weeks or longer.'
 				}
 			</ModalContent>
@@ -71,3 +79,21 @@ const WithdrawModal: React.FC<WithdrawModalProps> = ({
 }
 
 export default WithdrawModal
+
+const StyledDocsWarning = styled.span`
+	background-color: ${(props) => props.theme.color.grey[300]};
+	font-size: 16px;
+	margin: 1rem;
+	padding: 0.5rem;
+	text-align: start;
+	border-left: 3px solid ${(props) => props.theme.color.green};
+	width: 90%;
+`
+
+const StyledExternalLink = styled.a`
+	color: inherit;
+	margin: 2pt;
+	font-weight: 900;
+	text-decoration: underline;
+	display: inline;
+`
