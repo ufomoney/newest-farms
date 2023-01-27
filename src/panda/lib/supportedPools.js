50bb73459cb373cfe0758580169b1d1898b7c11f
ali3nid3als <ali3nid3als@gmail.com>
2023-01-27 23:30:54 +0300

    sec try to launch farms

--- src/panda/lib/supportedPools.js
+++ src/panda/lib/supportedPools.js
@@ -1,16 +1,17 @@
 export default [
 	{
-		pid: 3,
+		pid: 0,
 		lpAddresses: {
-			56: '0x97f6665ac6b2d7C3d5a2aD11d7a779787F617ce0',
+			56: '0x3B72Ac46888f72dD6a6B2076Ca96fcc18e75a935',
 		},
 		tokenAddresses: {
-			56: '0x47DcC83a14aD53Ed1f13d3CaE8AA4115f07557C0',
+			56: '0x3B72Ac46888f72dD6a6B2076Ca96fcc18e75a935',
 		},
 		tokenDecimals: 18,
 		name: 'Panda Party',
 		symbol: 'PNDA-BNB PNDA-V2',
 		tokenSymbol: 'PNDA',
+		poolType: PoolType.CAKE,
 		icon: '/pnda.png',
 		refUrl:
 			'https://pandaswap.xyz/#/swap?outputCurrency=0x47DcC83a14aD53Ed1f13d3CaE8AA4115f07557C0',
