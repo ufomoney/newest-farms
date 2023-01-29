8a3073361abb27ee424ed6affaa7f4bdb0e54ab2
ali3nid3als <ali3nid3als@gmail.com>
2023-01-29 18:55:33 +0300

    added new pool

--- src/panda/lib/supportedPools.js
+++ src/panda/lib/supportedPools.js
@@ -8,12 +8,29 @@ export default [
 			56: '0x3B72Ac46888f72dD6a6B2076Ca96fcc18e75a935',
 		},
 		tokenDecimals: 18,
-		name: 'Panda Party',
-		symbol: 'PNDA-BNB PNDA-V2',
-		tokenSymbol: 'PNDA',
+		name: 'UFO',
+		symbol: 'UFO',
+		tokenSymbol: 'UFO',
 		poolType: PoolType.ARCHIVED,
 		icon: '/pnda.png',
 		refUrl:
 			'https://pandaswap.xyz/#/swap?outputCurrency=0x47DcC83a14aD53Ed1f13d3CaE8AA4115f07557C0',
 	},
+	{
+		pid: 31,
+		lpAddresses: {
+		  56: '0xecb037cc672Fb2b53466Bbce986880149F79245B',
+		},
+		tokenAddresses: {
+		  56: '0xecb037cc672Fb2b53466Bbce986880149F79245B',
+		},
+		tokenDecimals: 18,
+		name: 'YUFO',
+		symbol: 'YUFO',
+		tokenSymbol: 'YUFO',
+		poolType: PoolType.PNDA,
+		icon: '/pnda.png',
+		refUrl:
+		  'https://pandaswap.xyz/#/swap?outputCurrency=0x47DcC83a14aD53Ed1f13d3CaE8AA4115f07557C0',
+	  },
 ]
